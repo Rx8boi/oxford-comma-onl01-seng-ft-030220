2b4c04353a2e837712d7802b671d39b3c1e3c93b
Charles Butler <cdbutle@outlook.com>
2020-03-12 03:48:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.size == 1
   return array[0]
 elsif array.size == 2 
 return array.join(" and ")
-elsif array.size == 3
-return array[0...-1].join(" , ") + "and" + array[-1]
+else array.size == 3
+return array[0..-1].join(" , ") + "and" + array[-1]
 end
 end
